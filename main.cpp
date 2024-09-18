e0568c3dd5ee7229632b0ba5e1cc223cc3718ae2
Elena Voronin <evoronin@f1r1s9.codam.nl>
2024-09-18 13:40:13 +0200

    main with some comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include "include/TcpServer.hpp"
 
-int main() {
-	return 0;
+
+int main(int argc, char **argv)
+{
+    Server server;
+
+    if (argc <= 2)
+    {
+    // if (argc == 1)
+        // we need is to use a default path
+    // if (argc == 2)
+        //parse configuration file
+    if (server.createServer(&server) == 0)
+        std::cout << "Server created" << std::endl;
+    else
+    {
+        std::cout << "Server creation failed" << std::endl;
+        return 1;
+    }
+    if (server.createClient(&server) == 0)
+        std::cout << "Client created" << std::endl;
+    else
+    {
+        std::cout << "Client creation failed" << std::endl;
+        return 1;
+    }
+    }
+    else
+    {
+		std::cerr << "Error: wrong number of arguments " << std::endl;
+		return 1;
+	}
+    }
+    else
+    {
+		std::cerr << "Error: wrong number of arguments " << std::endl;
+		return 1;
+	}
+
+    return 0;
 }
