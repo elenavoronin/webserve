39f3b57eec923db88aa548888f9fac5471b20e83
DjoykeAbyah <crudendjoyke@gmail.com>
2024-09-23 15:50:38 +0200

    start

---
+++ djoyke_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>       // For standard input/output operations
+#include <sys/types.h>    // Defines data types used in system calls (e.g., `sockaddr_in`, `socklen_t`)
+#include <sys/socket.h>   // Contains definitions for socket functions (e.g., `socket()`, `bind()`, `listen()`, `accept()`)
+#include <netinet/in.h>   // Contains constants and structures needed for internet domain addresses (e.g., `sockaddr_in`)
+#include <arpa/inet.h>    // Provides functions for manipulating numeric IP addresses (e.g., `inet_addr()`, `inet_ntoa()`)
+#include <unistd.h>       // Provides access to POSIX operating system API (e.g., `close()` for closing sockets)
+
+int main() {
+	//server fd?
+	return 0;
+}
