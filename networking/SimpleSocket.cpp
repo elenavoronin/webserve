797187d7b54de81f182fa24a43af8f23808756d7
DjoykeAbyah <crudendjoyke@gmail.com>
2024-09-23 17:03:45 +0200

    start done

--- networking/SimpleSocket.cpp
+++ networking/SimpleSocket.cpp
@@ -6,23 +6,53 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/23 16:02:20 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/09/23 16:26:56 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/09/23 17:03:32 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SimpleSocket.hpp"
 
-// why pass address,
-// what is sin_port
-// what is sin_family
-// what does htons do?
-// what does the connection_fd entail?
+/**
+ * why pass address,
+ * what is sin_port
+ * what is sin_family
+ * what does htons do?
+ * what does the sock entail?
+**/
+
+//Default constructor
 SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface){
 	//define address structure
-	address.sin_port = htons(port); 
-	address.sin_family = domain;
-	address.sin_addr.s_addr = htonl(interface);
-	//establish connection
-	connection_fd = socket(domain, service, protocol);
-	//bind?
+	_address.sin_port = htons(port); //replace by setters?
+	_address.sin_family = domain; //replace by setters?
+	_address.sin_addr.s_addr = htonl(interface); //replace by setters?
+	//establish socket
+	_sock = socket(domain, service, protocol); //replace by setters?
+	testConnection(_sock);
+	//establish network connection
+	_connection = connectToNetwork(_sock, _address);//call bind or connect
+	testConnection(_connection);
 }
+
+//test connection virtual function
+void SimpleSocket::testConnection(int item_to_test) {
+	//confirm if connection is established
+	if (item_to_test < 0) {
+		perror("Failed to connect");
+		exit(EXIT_FAILURE);
+	}
+}
+
+//getter functions
+struct sockaddr_in SimpleSocket::get_address() {
+	return _address;
+}
+
+int SimpleSocket::get_sock() {
+	return _sock;
+}
+
+int SimpleSocket::get_connection() {
+	return _connection;
+}
+
--- networking/SimpleSocket.hpp
+++ networking/SimpleSocket.hpp
@@ -6,7 +6,7 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/23 16:00:32 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/09/23 16:27:14 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/09/23 17:02:49 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 #include "sys/socket.h"
 #include <netinet/in.h>
+#include <iostream>
 
 // int server_fd = socket(domain, type, protocol);
 // domain = type of IP address
@@ -23,16 +24,23 @@
 class SimpleSocket {
 	private:
 
-		struct sockaddr_in 	address;
-		int 				connection_fd;
+		struct sockaddr_in 	_address;
+		int 				_sock;
+		int					_connection;
 		
 	public:
-	
+		
 		SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
 		SimpleSocket(const SimpleSocket &copy);
 		SimpleSocket& operator=(const SimpleSocket &copy);
 		~SimpleSocket();
 
-		void setSimpleSocket();
-		?? getSimpleSocket();
+		//virtual fucntion to connect to a network
+		virtual int 		connectToNetwork(int sock, struct sockaddr_in address) = 0;
+		//function to test sockets and connections
+		void 				testConnection(int); //maybe virtual function?
+		//getter functions
+		struct sockaddr_in 	get_address();
+		int 				get_sock();
+		int 				get_connection();
 };
