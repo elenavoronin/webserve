e62762cefef485805c0c88307767573f766d3ad6
DjoykeAbyah <crudendjoyke@gmail.com>
2025-01-29 15:46:17 +0100

    two servers working do not close connection if client is not there

--- src/Server.cpp
+++ src/Server.cpp
@@ -141,7 +141,7 @@ void Server::handlePollEvent(EventPoll &eventPoll, int i, Server& defaultServer)
 
     if (!client) {
         std::cerr << "Client not found for fd: " << event_fd << std::endl;
-		client->closeConnection(eventPoll);
+		//client->closeConnection(eventPoll);
 		eraseClient(event_fd);
         return;
     }
